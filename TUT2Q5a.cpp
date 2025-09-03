9437184b08ba7cde7e8d70e24cf2be566dda50e6
sakshamytu <sgrover_be24@thapar.edu>
2025-09-03 21:50:34 +0530

    Update TUT2Q5a.cpp

--- TUT2Q5a.cpp
+++ TUT2Q5a.cpp
@@ -1,55 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-
-    cout<<"enter the size of the square matrix : ";
+int main() {
     int n;
-    cin>>n;
+    cout << "Enter size: ";
+    cin >> n;
 
-    int diag_mat[n];
+    int diag[n]; 
 
-    for (int i = 0; i < n; i++)
-    {
-        cout<<"enter the element for "<<i+1<<" row diagonal : ";
-        cin>>diag_mat[i];
-    }
     
-    cout<<"the array for diagonal element matrix created is :"<<endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout<<diag_mat[i]<<endl;
+    cout << "Enter diagonal elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> diag[i];
     }
+
     
-    // diagonal matrix in it's full form
-
-    int mat[n][n];
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i==j)
-            {
-                mat[i][j]=diag_mat[i];
-            }
-            else{
-                mat[i][j]=0;
-            }
+    cout << "Matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << ((i == j) ? diag[i] : 0) << " ";
         }
-        
+        cout << endl;
     }
-    
-    // the diagonal matrix is :
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n ; j++)
-        {
-            cout<<mat[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    
-
 }
